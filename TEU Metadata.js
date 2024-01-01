9c027746e63a14674f5e9f5e17fc9f9dd56ca6c6
haile6886 <40747022+haile6886@users.noreply.github.com>
2024-01-01 09:53:32 +0700

    Add files via upload

---
+++ TEU Metadata.js
@@ -0,0 +1,6 @@
+{
+  "name": "TEU Meme Coin",
+  "symbol": "TEU",
+  "description": "Blue Algi for Algotrading",
+  "image": "https://raw.githubusercontent.com/haile6886/TEU-Meme-Coin/main/TEU_Logo_500x500.png"
+}
